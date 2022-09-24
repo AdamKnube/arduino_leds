66bc85fbbbca88679da8d1d1fa937221006b5375
AdamKnube <adamk@5csecurity.com>
2022-09-24 15:36:39 -0400

    modified:   LED_TEST.ino - The grandkids wanted chasers and the ability to change colours on-the-fly.

--- LED_TEST.ino
+++ LED_TEST.ino
@@ -1,10 +1,17 @@
+#include <string.h>
 #include <FastLED.h>
 #define DEBUG
-#define EYE_BURN
+#define CHASER
+#define SEBASTIEN
+//#define EYE_BURN
+#define VCC_PIN 0
 #define LED_PIN 26
 #define DELAY_TIME 25
 #define TOTAL_LEDS 300
 CRGB led_strip[TOTAL_LEDS];
+#ifdef SEBASTIEN
+CRGB::HTMLColorCode seb_colours[4] = { CRGB::HTMLColorCode::White, CRGB::HTMLColorCode::Blue, CRGB::HTMLColorCode::Purple, CRGB::HTMLColorCode::Red };
+#endif
 
 void setup() {
   // put your setup code here, to run once:
@@ -15,6 +22,33 @@ void setup() {
 }
 
 void led_reset(int colour = 0) { memset(led_strip, colour, TOTAL_LEDS * 3); }
+
+void dprint(char* message = "", bool newline = false) {
+#ifdef DEBUG  
+  if (newline) { Serial.println(message); }
+  else { Serial.print(message); }
+#else
+  return;
+#endif
+}
+
+void show_voltage() {
+  float vcc = analogRead(VCC_PIN);   
+  int val1 = vcc / 3.9;
+  int val5 = (int)val1;
+  int val3 = val5 / 100;
+  int val2 = (val5 % 100) / 10;
+  int val4 = val5 % 10;
+  char vccstr[32];
+  dprint("Raw VCC: ");
+  itoa(val3, vccstr, 10);
+  dprint(vccstr);
+  itoa(val2, vccstr, 10);
+  dprint(vccstr);
+  dprint(".");
+  itoa(val4, vccstr, 10);
+  dprint(vccstr, true);  
+}
 
 void ramp_up(int start = 0, int end = TOTAL_LEDS - 1, int timing = DELAY_TIME) {
   int num_leds = end - start;
@@ -58,9 +92,44 @@ void particle_collide(int start = 0, int end = TOTAL_LEDS - 1, int timing = DELA
     else if (count < changeover) { this_colour = CRGB::Red; }
     else { this_colour = CRGB::Blue; }   
 #endif
+#ifdef SEBASTIEN
+    led_strip[start + count] = seb_colours[0];
+    led_strip[end - count] = seb_colours[0];
+#else
     led_strip[start + count] = this_colour;
     led_strip[end - count] = this_colour;
+#endif
+#ifdef CHASER    
+    if (count > 0) { 
+  #ifdef SEBASTIEN
+      led_strip[start + count - 1] = seb_colours[1]; 
+      led_strip[end - count + 1] = seb_colours[1];
+  #else
+      led_strip[start + count - 1] = this_colour; 
+      led_strip[end - count + 1] = this_colour;
+  #endif
+    }
+    if (count > 1) { 
+  #ifdef SEBASTIEN
+      led_strip[start + count - 2] = seb_colours[2]; 
+      led_strip[end - count + 2] = seb_colours[2];
+  #else
+      led_strip[start + count - 2] = this_colour; 
+      led_strip[end - count + 2] = this_colour;
+  #endif
+    }
+    if (count > 2) { 
+  #ifdef SEBASTIEN      
+      led_strip[start + count - 3] = seb_colours[3]; 
+      led_strip[end - count + 3] = seb_colours[3];
+  #else
+      led_strip[start + count - 3] = this_colour; 
+      led_strip[end - count + 3] = this_colour;
+  #endif
+    }
+#endif
     FastLED.show();
+    show_voltage();
     delay(timing);    
   }
 }
@@ -83,25 +152,58 @@ void particle_explode(int start = 0, int end = TOTAL_LEDS - 1, int timing = DELA
     else if (count < changeover) { this_colour = CRGB::Green; }
     else { this_colour = CRGB::Blue; }   
 #endif    
+#ifdef SEBASTIEN      
+    led_strip[start + collision + count] = seb_colours[0];
+    led_strip[end - collision - count] = seb_colours[0];
+#else
     led_strip[start + collision + count] = this_colour;
     led_strip[end - collision - count] = this_colour;
+#endif
+#ifdef CHASER    
+    if (count > 0) { 
+  #ifdef SEBASTIEN            
+      led_strip[start + collision + count - 1] = seb_colours[1];
+      led_strip[end - collision - count + 1] = seb_colours[1];
+  #else
+      led_strip[start + collision + count - 1] = this_colour;
+      led_strip[end - collision - count + 1] = this_colour;
+  #endif
+    }
+    if (count > 1) { 
+  #ifdef SEBASTIEN            
+      led_strip[start + collision + count - 2] = seb_colours[2];
+      led_strip[end - collision - count + 2] = seb_colours[2];
+  #else
+      led_strip[start + collision + count - 2] = this_colour;
+      led_strip[end - collision - count + 2] = this_colour;
+  #endif
+    }
+    if (count > 2) { 
+  #ifdef SEBASTIEN            
+      led_strip[start + collision + count - 3] = seb_colours[3];
+      led_strip[end - collision - count + 3] = seb_colours[3];
+  #else
+      led_strip[start + collision + count - 3] = this_colour;
+      led_strip[end - collision - count + 3] = this_colour;
+  #endif
+    }
+#endif
     FastLED.show();
+    show_voltage();
     delay(timing);    
   }
 }
 
 void blow_shit_up(int start = 0, int end = TOTAL_LEDS - 1, int timing = DELAY_TIME) {
-  for (int i = 0; i < 6; i++){
-    particle_collide();
-    particle_explode();
-  }  
+  particle_collide(start, end, timing);
+  particle_explode(start, end, timing);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   //ramp_up();
   //ramp_down();
-  particle_collide();
-  particle_explode();
-  //blow_shit_up();
+  //particle_collide();
+  //particle_explode();
+  blow_shit_up();
 }
