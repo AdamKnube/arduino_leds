2b4f127e75df7be506e51bf1a9b7e85a44d0cc56
AdamKnube <adamk@5csecurity.com>
2022-10-04 16:47:46 -0400

    modified:   LED_TEST.ino - Code clean-up (sorry seb). 				 - Introduced 16x2 LCD output for voltages.

--- LED_TEST.ino
+++ LED_TEST.ino
@@ -1,42 +1,28 @@
-#include <string.h>
 #include <FastLED.h>
+#include <LiquidCrystal.h>
 #define DEBUG
-#define HARRY
-#define CHASER
-//#define OLD_SKOOL
-//#define SEBASTIEN
-#define V05_PIN 0
-#define V12_PIN 1
+#define V05_PIN A0
+#define V12_PIN A1
 #define LED_PIN 26
 #define DELAY_TIME 25
 #define TOTAL_LEDS 300
 CRGB led_strip[TOTAL_LEDS];
-#ifdef SEBASTIEN
-CRGB::HTMLColorCode seb_colours[4] = { CRGB::HTMLColorCode::Green, CRGB::HTMLColorCode::Red, CRGB::HTMLColorCode::Blue, CRGB::HTMLColorCode::Orange };
-#endif
-#ifdef HARRY
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 CRGB::HTMLColorCode seb_colours[4] = { CRGB::HTMLColorCode::White, CRGB::HTMLColorCode::Yellow, CRGB::HTMLColorCode::Green, CRGB::HTMLColorCode::Blue };
-#endif
-#ifdef OLD_SKOOL
-CRGB::HTMLColorCode seb_colours[4] = { CRGB::HTMLColorCode::White, CRGB::HTMLColorCode::Blue, CRGB::HTMLColorCode::Orange, CRGB::HTMLColorCode::Red };
-#endif
-
-bool firstRun = true;
 
 void setup() {
   // put your setup code here, to run once:
+  lcd.begin(16, 2);
+#ifdef DEBUG
   Serial.begin(9600);
+#endif
   pinMode(V05_PIN, INPUT); 
   pinMode(V12_PIN, INPUT); 
-  pinMode(LED_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);    
   FastLED.addLeds<NEOPIXEL, LED_PIN>(led_strip, TOTAL_LEDS);
 }
 
-#ifdef SEBASTIEN
-void led_reset(int colour = 1) { memset(led_strip, colour, TOTAL_LEDS * 3); }
-#else
 void led_reset(int colour = 0) { memset(led_strip, colour, TOTAL_LEDS * 3); }
-#endif
 
 void dprint(char message[] = "", bool newline = false) {
 #ifdef DEBUG  
@@ -69,31 +55,30 @@ float fmap(float x, float in_min, float in_max, float out_min, float out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-void show_voltage(bool csv = true) {
+void show_voltage() {  
   int v05 = analogRead(V05_PIN);   
   int v12 = analogRead(V12_PIN);
-  if (csv) {
-    if (firstRun) { 
-      Serial.println("5VDC Bus,12VDC Bus");
-      firstRun = false;
-    }
-    Serial.print(fmap(v05, 0, 1024, 0, 26));
-    Serial.print("v,");
-    Serial.print(fmap(v12, 0, 1024, 0, 26));
-    Serial.println("v");
-  }
-  else {
-    dprint("RAW 5V: ");
-    dprint(v05);
-    dprint(", V05: ");
-    dprint(fmap(v05, 0, 1024, 0, 26));    
-    dprint("v, RAW 12V: ");
-    dprint(v12);
-    dprint(", V12: ");
-    dprint(fmap(v12, 0, 1024, 0, 26));        
-    dprint("v.", true);
-  }
+  lcd.setCursor(0, 0);
+  lcd.print("5vDC Bus: ");
+  lcd.print(fmap(v05, 0, 1024, 0, 26));
+  lcd.print("v");
+  lcd.setCursor(0, 1);
+  lcd.print("12vDC Bus: ");
+  lcd.print(fmap(v12, 0, 1024, 0, 26));
+  lcd.print("v");
+#ifdef DEBUG
+  dprint("RAW 5V: ");
+  dprint(v05);
+  dprint(", V05: ");
+  dprint(fmap(v05, 0, 1024, 0, 26));    
+  dprint("v, RAW 12V: ");
+  dprint(v12);
+  dprint(", V12: ");
+  dprint(fmap(v12, 0, 1024, 0, 26));        
+  dprint("v.", true);
+#endif
 }
+
 
 void ramp_up(int start = 0, int end = TOTAL_LEDS - 1, int timing = DELAY_TIME) {
   int num_leds = end - start;
@@ -126,15 +111,8 @@ void particle_collide(int start = 0, int end = TOTAL_LEDS - 1, int timing = DELA
   for (int count = 0; count <= collision; count++) {
     led_reset();
     CRGB::HTMLColorCode this_colour = seb_colours[0];    
-#ifdef OLD_SKOOL
-    if (count >= collision - 1) { this_colour = seb_colours[0]; }
-    else if (count > changeover * 2) { this_colour = seb_colours[3]; }
-    else if (count < changeover) { this_colour = seb_colours[1]; }
-    else { this_colour = seb_colours[2]; }   
-#endif
     led_strip[start + count] = this_colour;
     led_strip[end - count] = this_colour;
-#ifdef CHASER    
     if (count > 0) { 
       led_strip[start + count - 1] = seb_colours[1]; 
       led_strip[end - count + 1] = seb_colours[1];
@@ -147,9 +125,8 @@ void particle_collide(int start = 0, int end = TOTAL_LEDS - 1, int timing = DELA
       led_strip[start + count - 3] = seb_colours[3]; 
       led_strip[end - count + 3] = seb_colours[3];
     }
-#endif
     FastLED.show();
-    show_voltage(false);
+    show_voltage();
     delay(timing);    
   }
 }
@@ -161,15 +138,8 @@ void particle_explode(int start = 0, int end = TOTAL_LEDS - 1, int timing = DELA
   for (int count = 0; count <= collision; count++) {
     led_reset();
     CRGB::HTMLColorCode this_colour = seb_colours[0];    
-#ifdef OLD_SKOOL
-    if (count <= 1) { this_colour = seb_colours[0]; }
-    else if (count > changeover * 2) { this_colour = seb_colours[3]; }
-    else if (count < changeover) { this_colour = seb_colours[1]; }
-    else { this_colour = seb_colours[2]; }   
-#endif
     led_strip[start + collision + count] = this_colour;
     led_strip[end - collision - count] = this_colour;
-#ifdef CHASER    
     if (count > 0) { 
       led_strip[start + collision + count - 1] = seb_colours[1];
       led_strip[end - collision - count + 1] = seb_colours[1];
@@ -182,9 +152,8 @@ void particle_explode(int start = 0, int end = TOTAL_LEDS - 1, int timing = DELA
       led_strip[start + collision + count - 3] = seb_colours[3];
       led_strip[end - collision - count + 3] = seb_colours[3];
     }
-#endif
     FastLED.show();
-    show_voltage(false);
+    show_voltage();
     delay(timing);    
   }
 }
@@ -202,3 +171,5 @@ void loop() {
   //particle_explode();
   blow_shit_up();
 }
+
+
